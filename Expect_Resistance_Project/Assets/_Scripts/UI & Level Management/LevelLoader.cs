323cea8cd27b77801c67eccba4f5c592e495404e
CKTA00 <przekozuch@gmail.com>
2022-05-21 17:23:33 +0200

    Add auto-generated level selection menu
    Add empty levels for testing purposes

--- Expect_Resistance_Project/Assets/_Scripts/UI & Level Management/LevelLoader.cs
+++ Expect_Resistance_Project/Assets/_Scripts/UI & Level Management/LevelLoader.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] GameObject levelSeclectionGridPanel;
+    [SerializeField] GameObject levelButtonPrefab;
+    public const string BASE_LEVEL_NAME = "level_";
+
+    void Start()
+    {
+        CreateSelectLevelMenu();
+    }
+
     public void LoadLevel(int i)
     {
-        string sceneName = "level_" + i;
+        string sceneName = BASE_LEVEL_NAME + i;
         if(Application.CanStreamedLevelBeLoaded(sceneName))
         {
             SceneManager.LoadScene(sceneName);
@@ -22,5 +32,21 @@ public class LevelLoader : MonoBehaviour
     public void LoadTestLevel()
     {
         SceneManager.LoadScene("TestLevel");
+    }
+
+    public void CreateSelectLevelMenu()
+    {
+        int levelIndex = 1;
+        string sceneName = BASE_LEVEL_NAME + levelIndex;
+        while(Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            GameObject currentButton = Instantiate(levelButtonPrefab, levelSeclectionGridPanel.transform);
+            currentButton.GetComponentInChildren<Text>().text = levelIndex.ToString();
+            int levelIndex_ = levelIndex; // this is necessary!
+            currentButton.GetComponent<Button>().onClick.AddListener(() => { LoadLevel(levelIndex_); });
+
+            levelIndex++;
+            sceneName = BASE_LEVEL_NAME + levelIndex;
+        }
     }
 }
